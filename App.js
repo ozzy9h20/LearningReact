3aacc17ac0898926245868649645e69b7c8d458b
Endyarroziqin <111201810980@mhs.dinus.ac.id>
2020-11-18 09:25:39 +0700

    v6.17
    Menambahkan Komponen NavBar

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
+import React from 'react';
 import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import "./App.css";
+import NavBar from "./components/navBar";
 
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect exact from="/" to="/Movies" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect exact from="/" to="/Movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
