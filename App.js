5078745118e2ba7a493bdf3d4648d394eccda204
Endyarroziqin <111201810980@mhs.dinus.ac.id>
2020-11-18 09:03:56 +0700

    Revert "v6.16"
    This reverts commit 2feb4ca5aa22315d657ad499d429bc1b5c46a12d.

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
-import Customers from './components/customers';
-import Rentals from './components/rentals';
-import NotFound from './components/notFound';
 import "./App.css";
 
 function App() {
   return (
     <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect exact from="/" to="/Movies" />
-        <Redirect to="/not-found" />
-      </Switch>
+      <Movies />
     </main>
   );
 }
