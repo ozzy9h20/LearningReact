9c352b105211e61d9732be4a02ea8016d18af9ff
Endyarroziqin <111201810980@mhs.dinus.ac.id>
2020-11-18 23:30:01 +0700

    v7.02
    - Routing to Login
    - Building Bootstrap Form

--- App.js
+++ App.js
@@ -5,8 +5,9 @@ import MovieForm from "./components/movieForm";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
-import "./App.css";
 import NavBar from "./components/navBar";
+import LoginForm from "./components/loginForm";
+import "./App.css";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id/" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
