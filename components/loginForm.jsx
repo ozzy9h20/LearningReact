2fabd88f3e4ff4e268c074ee38d48466bfcef217
Endyarroziqin <111201810980@mhs.dinus.ac.id>
2020-11-18 23:34:37 +0700

    v7.03
    Handling Form Submission
    using preventDefault() to remove default action of form submit

--- components/loginForm.jsx
+++ components/loginForm.jsx
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("Submitted");
+  };
+
   render() {
-    return (<div>
-      <h1>Login Form</h1>
-      <form action="">
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input id="username" type="text" className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" className="form-control" />
-        </div>
-        <button className="btn btn-primary">Submit</button>
-      </form>
-    </div>);
+    return (
+      <div>
+        <h1>Login Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Submit</button>
+        </form>
+      </div>);
   }
 }
 
