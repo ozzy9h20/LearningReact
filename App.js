830e52e495ba4740a6d578e974dae44ad09dbd8f
Endyarroziqin <111201810980@mhs.dinus.ac.id>
2020-11-18 23:13:55 +0700

    v6.18
    Membuat link pada elemen tabel

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
-import Customers from './components/customers';
-import Rentals from './components/rentals';
-import NotFound from './components/notFound';
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 import "./App.css";
 import NavBar from "./components/navBar";
 
@@ -12,12 +13,12 @@ function App() {
     <React.Fragment>
       <NavBar />
       <main className="container">
-
         <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
+          <Route path="/movies/:id/" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
           <Redirect exact from="/" to="/Movies" />
           <Redirect to="/not-found" />
         </Switch>
