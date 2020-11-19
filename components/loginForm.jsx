8e8b62f8d123bef190ab7d820ee918d54bc640b2
Endyarroziqin <111201810980@mhs.dinus.ac.id>
2020-11-19 16:00:52 +0700

    v7.10
    Validasi form kosong (simple)

--- components/loginForm.jsx
+++ components/loginForm.jsx
@@ -9,19 +9,31 @@ class LoginForm extends Component {
   }
 
   validate = () => {
-    return { username: 'Username is required.' }
+    const errors = {};
+
+    const { account } = this.state;
+    if (account.username.trim() === '') {
+      errors.username = 'Username is required.';
+    }
+    if (account.password.trim() === '') {
+      errors.password = 'Password is required.';
+    }
+
+    return Object.keys(errors).length === 0
+      ? null
+      : errors;
   }
 
   handleSubmit = e => {
     e.preventDefault();
 
     const errors = this.validate();
+    console.log(errors);
     this.setState({ errors });
 
     if (errors) return;
 
-    const username = this.username.current.value;
-    console.log(username);
+    console.log("Submitted");
   };
 
   handleChange = ({ currentTarget: input }) => {
