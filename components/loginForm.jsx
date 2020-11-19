3214789b20e25b782e5c92877e8fbb9d26b769df
Endyarroziqin <111201810980@mhs.dinus.ac.id>
2020-11-19 21:54:28 +0700

    v7.21 Adding Register Module
    - Nambah Route di App.js
    - Nambah NavLink di nabvBar.jsx
    - Buat komponen registerForm.jsx

--- components/loginForm.jsx
+++ components/loginForm.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Joi from 'joi-browser';
-import Input from './common/input';
 import Form from './common/form';
 
 class LoginForm extends Form {
