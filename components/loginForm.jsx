8e0838aaf4cc8e7207b4dbe78271c2540d5d91e4
Endyarroziqin <111201810980@mhs.dinus.ac.id>
2020-11-19 16:08:48 +0700

    v7.11
    Showing errors

--- components/loginForm.jsx
+++ components/loginForm.jsx
@@ -28,9 +28,7 @@ class LoginForm extends Component {
     e.preventDefault();
 
     const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors });
-
+    this.setState({ errors: errors || {} });
     if (errors) return;
 
     console.log("Submitted");
@@ -43,7 +41,7 @@ class LoginForm extends Component {
   }
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login Form</h1>
@@ -53,12 +51,14 @@ class LoginForm extends Component {
             value={account.username}
             label="username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Submit</button>
         </form>
