146636f1adf3f0df6d107b14f87fe7bff040db81
Endyarroziqin <111201810980@mhs.dinus.ac.id>
2020-11-19 16:37:32 +0700

    v7.14 Joi Form Validation

--- components/loginForm.jsx
+++ components/loginForm.jsx
@@ -10,32 +10,24 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required()
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password")
   };
 
   validate = () => {
-    const result = Joi.validate(
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(
       this.state.account,
       this.schema,
-      { abortEarly: false }
+      options
     );
 
-    console.log(result);
+    if (!error) return null;
 
     const errors = {};
-
-    const { account } = this.state;
-    if (account.username.trim() === '') {
-      errors.username = 'Username is required.';
-    }
-    if (account.password.trim() === '') {
-      errors.password = 'Password is required.';
-    }
-
-    return Object.keys(errors).length === 0
-      ? null
-      : errors;
+    for (let item of error.details)
+      errors[item.path[0]] = item.message;
+    return errors;
   }
 
   handleSubmit = e => {
