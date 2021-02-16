286359082f95ef8003cef93ca23c0764185493d1
Endyarroziqin <111201810980@mhs.dinus.ac.id>
2021-02-16 10:56:26 +0700

    v8.28 Refactor MovieForm

--- services/movieService.js
+++ services/movieService.js
@@ -7,6 +7,14 @@ export function getMovies() {
   return http.get(apiEndpoint);
 }
 
+export function getMovie(movieId) {
+  return http.get(`${apiEndpoint}/${movieId}`);
+}
+
+export function saveMovie(movie) {
+  // return http.put(`${apiEndpoint}/${movieId}`);
+}
+
 export function deleteMovie(movieId) {
   return http.delete(`${apiEndpoint}/${movieId}`);
 }
