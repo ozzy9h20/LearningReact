79069d55ff88e08bbb4b3f32bc4324e4c73fd0cc
Endyarroziqin <111201810980@mhs.dinus.ac.id>
2021-02-17 01:30:52 +0700

    v9.0.2 Register & Validation

--- components/registerForm.jsx
+++ components/registerForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from './common/form';
 import Joi from 'joi-browser';
+import * as userService from './../services/userService';
 
 class RegisterForm extends Form {
   state = {
@@ -26,9 +27,18 @@ class RegisterForm extends Form {
       .label("Name")
   };
 
-  doSubmit = () => {
+  doSubmit = async () => {
     // Calling the server
-    console.log("Submitted");
+    // console.log("Submitted");
+    try {
+      await userService.register(this.state.data);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   }
 
   render() {
